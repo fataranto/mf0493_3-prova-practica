fbfbb7ac3cbc9e4abf75fbb34919dbc9cdc060d6
fataranto <fataranto@gmail.com>
2022-09-15 15:56:28 +0200

    ex ok

--- unit-testing/isogram.test.js
+++ unit-testing/isogram.test.js
@@ -1,3 +1,17 @@
 import {it, expect, describe} from 'vitest';
 import { isIsogram } from './isogram';
 
+describe('isIsogram', () => {
+  it('should return true if the string is an isogram', () => {
+    expect(isIsogram('Dermatoglyphics')).toBe(true); 
+  });
+
+    it('should return false if the string is not an isogram', () => {
+    expect(isIsogram('aba')).toBe(false); 
+  });
+
+    it('should return false if the string with mixed case is not an isogram', () => {
+    expect(isIsogram('moOse')).toBe(false); 
+  });
+
+});
